f5b3feca9102598e0110a4e450d5b810407f71d4
Denis Golodkov <gold8523@gmail.com>
2016-12-04 22:53:57 +0300

    commit 3 ДЗ 6 Twig

--- core/view.php
+++ core/view.php
@@ -1,9 +1,30 @@
 <?php
+require_once dirname(__DIR__).'/vendor/twig/twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
 
 class View {
 
-    public function render($content_view, $name, $data = null)
+    private $twig;
+
+    function __construct()
     {
-        require 'views/' . $name;
+        $loader = new Twig_Loader_Filesystem(dirname(__DIR__).'/views/twig');
+        $twig = new Twig_Environment($loader, array(
+            'cache' => false
+        ));
+        $this->twig = $twig;
+
     }
+
+//    public function render($content_view, $name, $data = null)
+//    {
+//        require 'views/' . $name;
+//    }
+
+    function generate($content_view, $data = []) {
+
+        echo $this->twig->render($content_view, $data);
+
+    }
+
 }
